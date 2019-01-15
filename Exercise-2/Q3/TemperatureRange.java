9daa245d37bf3d3558286d2a9617d14bbdfc0963
Patrick Wu <magetron1@gmail.com>
2019-01-15 23:01:59 +0000

    finished Exercise 2 Q3

--- Exercise-2/Q3/TemperatureRange.java
+++ Exercise-2/Q3/TemperatureRange.java
@@ -4,12 +4,13 @@ public class TemperatureRange {
 	
 	private int lower;
 	private int upper;
-	private String tableHeader = "";
+	private String tableHeader = "Default Table Header";
 	private String celciusColumnHeader = "C";
 	private String fahrenheitColumnHeader = "F";
-	private int numberOfColumns;
-	private int numberOfSpacesBetween;
-	private ArrayList<String> temperatureTable = new ArrayList<>();
+	private int columnWidth = 4;
+	private int numberOfColumns = 4;
+	private int numberOfSpacesBetween = 5;
+	private ArrayList< ArrayList<String> > temperatureTable = new ArrayList< ArrayList<String> >();
 
 	TemperatureRange (int lower, int upper) throws Exception {
 		if (lower > upper) throw new Exception ("ERROR : invalid TemperatureRange.");
@@ -17,44 +18,95 @@ public class TemperatureRange {
 		this.upper = upper;
 	}
 
+	public String getTableHeader () {
+		return tableHeader;
+	}
+
+	public int getColumnWidth () {
+		return columnWidth;
+	}
+
+	public int getNumberOfSpacesBetweenColumns () {
+		return numberOfSpacesBetween;
+	}
+
+	public int getNumberOfColumns () {
+		return numberOfColumns;
+	}
+	
 	public void setHeader (String inputHeader) {
-		this.tableHeader = inputHeader;
+		tableHeader = inputHeader;
 	}
 
 	public void setCelciusColumnHeader (String celciusHeader) {
-		this.celciusColumnHeader = celciusHeader;
+		celciusColumnHeader = celciusHeader;
 	}
 
 	public void setFahrenheitColumnHeader (String fahrenheitHeader) {
-		this.fahrenheitColumnHeader = fahrenheitHeader;
+		fahrenheitColumnHeader = fahrenheitHeader;
 	}
 
 	public void setNumberOfColumns (int inputNumber) throws Exception {
 		if (inputNumber < 1) throw new Exception ("ERROR : invalid numberOfColumns.");
-		this.numberOfColumns = inputNumber;
+		numberOfColumns = inputNumber;
 	}
 
 	public void setNumberOfSpacesBetween (int inputNumber) throws Exception {
 		if (inputNumber < 0) throw new Exception ("ERROR : invalid numberOfSpacesBetween.");
-		this.numberOfSpacesBetween = inputNumber;
+		numberOfSpacesBetween = inputNumber;
 	}
 
 	private int toFahrenheit (int celcius) {
-		return Math.toIntExact(Math.round(celcius * 9.0 / 5 - 32));
+		return Math.toIntExact(Math.round(celcius * 9.0 / 5 + 32));
+	}
+
+	private ArrayList<String> generateTableHeader () {
+		ArrayList<String> tableHeaderArray = new ArrayList<String>();
+		tableHeaderArray.add(tableHeader);
+		String lineString = "";
+		for (int i = 0; i < tableHeader.length(); i++) lineString += "-";
+		tableHeaderArray.add(lineString);
+		return tableHeaderArray;
+	}
+
+	private ArrayList<String> generateTableColumns () {
+		ArrayList<String> tableColumnArray = new ArrayList<String>();
+		String singleColumnString = "";
+		singleColumnString += celciusColumnHeader;
+		for (int i = 0; i < columnWidth - celciusColumnHeader.length(); i++) 
+			singleColumnString += " ";
+		singleColumnString += fahrenheitColumnHeader;
+		for (int i = 0; i < columnWidth - fahrenheitColumnHeader.length(); i++)
+			singleColumnString += " ";
+		for (int i = 0; i < numberOfColumns; i++) tableColumnArray.add(singleColumnString);
+		return tableColumnArray;
+	}
+
+	private ArrayList<String> generateTableContents() {
+		ArrayList<String> tableContentArray = new ArrayList<String>();
+		for (int i = lower; i <= upper; i++) {
+			String temperatureString = "";
+			String celciusString = Integer.toString(i);
+			temperatureString += celciusString;
+			for (int j = 0; j < columnWidth - celciusString.length(); j++)
+				temperatureString += " ";
+			String fahrenheitString = Integer.toString(toFahrenheit(i));
+			temperatureString += fahrenheitString;
+			for (int j = 0; j < columnWidth - fahrenheitString.length(); j++) 
+				temperatureString += " ";
+			tableContentArray.add(temperatureString);
+		}
+		return tableContentArray;
 	}
 
 	public void generateTable () {
-		temperatureTable.add(this.tableHeader);
-		String lineString = "";
-		for (int i = 0; i < this.tableHeader.length(); i++) lineString += "-";
-		temperatureTable.add(lineString);
-		temperatureTable.add(String.format("%4s%4s",celciusColumnHeader, fahrenheitColumnHeader));
-		for (int i = lower; i <= upper; i++)
-			temperatureTable.add(String.format("%4d%4d", i, toFahrenheit(i)));
+		temperatureTable.add(generateTableHeader());
+		temperatureTable.add(generateTableColumns());
+		temperatureTable.add(generateTableContents());
 	}
 
-	public ArrayList<String> getTable () {
-		return this.temperatureTable;
+	public ArrayList< ArrayList<String> > getTable () {
+		return temperatureTable;
 	}
 }
 
--- Exercise-2/Q3/TemperatureRangeCLI.java
+++ Exercise-2/Q3/TemperatureRangeCLI.java
@@ -9,6 +9,11 @@ public class TemperatureRangeCLI {
 		return in.nextInt();
 	}
 
+	private String getString () {
+		Input in = new Input();
+		return in.nextLine();
+	}
+
 	private void printInstructions () {
 		System.out.println("1. Set Temperature Range");
 		System.out.println("2. Set Table Header");
@@ -17,16 +22,107 @@ public class TemperatureRangeCLI {
 		System.out.println("5. Set Number of Columns");
 		System.out.println("6. Set Number of Spaces Between Columns");
 		System.out.println("7. Output Temperature Table");
+		System.out.println("8. Quit");
 	}
 
-	private TemperatureRange setNewTemperatureRange (int lower, int upper) {
+	private void setNewTemperatureRange (int lower, int upper) {
 		try {
-			return new TemperatureRange(lower, upper);
+			temperatureRange = new TemperatureRange(lower, upper);
 		} catch (Exception invalidTemperatureRange) {
 			System.out.println("ERROR : invalid Temperature Range.");
 		}
 	}
 
+	private void setTableHeader (String newTableHeader) {
+		temperatureRange.setHeader(newTableHeader);
+	}
+
+	private void setTableCelciusColumnHeader (String newCelciusColumnHeader) {
+		temperatureRange.setCelciusColumnHeader(newCelciusColumnHeader);
+	}
+
+	private void setTableFahrenheitColumnHeader (String newFahrenheitColumnHeader) {
+		temperatureRange.setFahrenheitColumnHeader(newFahrenheitColumnHeader);
+	}
+
+	private void setNumberOfColumns (int newNumberOfColumns) {
+		try {
+			temperatureRange.setNumberOfColumns(newNumberOfColumns);
+		} catch (Exception invalidNumberOfColumns) {
+			System.out.println("ERROR : invalid Number Of Columns.");
+		}
+	}
+
+	private void setNumberOfSpacesBetween (int newNumberOfSpacesBetween) {
+		try {
+			temperatureRange.setNumberOfSpacesBetween(newNumberOfSpacesBetween);
+		} catch (Exception invalidNumberOfSapcesBetween) {
+			System.out.println("ERROR : invalid Number of Spaces Between");
+		}
+	}
+
+	private void outputTemperatureTable ( ArrayList< ArrayList<String> > table ) {
+		//System.out.println(table);
+		ArrayList<String> header = table.get(0);
+		ArrayList<String> columns = table.get(1);
+		ArrayList<String> contents = table.get(2);
+		int columnWidth = temperatureRange.getColumnWidth();
+		int numberOfSpacesBetweenColumns = temperatureRange.getNumberOfSpacesBetweenColumns();
+		int numberOfColumns = temperatureRange.getNumberOfColumns();
+		int lineWidth = numberOfColumns * columnWidth * 2 + numberOfSpacesBetweenColumns * (numberOfColumns - 1);
+		int headerOffset = (lineWidth - temperatureRange.getTableHeader().length()) / 2;
+		for (int i = 0; i < header.size(); i++) {
+			for (int j = 0; j < headerOffset; j++) System.out.print(" ");
+			System.out.println(header.get(i));
+		}
+		for (int i = 0; i < columns.size(); i++) {
+			System.out.print(columns.get(i));
+			for (int j = 0; j < numberOfSpacesBetweenColumns; j++)
+				System.out.print(" ");
+		}
+		System.out.println();
+		int currentColumn = 0;
+		for (int i = 0; i < contents.size(); i++) {
+			System.out.print(contents.get(i));
+			for (int j = 0; j < numberOfSpacesBetweenColumns; j++)
+				System.out.print(" ");
+			currentColumn++;
+			if (currentColumn == numberOfColumns) {
+				System.out.println();
+				currentColumn = 0;
+			}
+		}
+		System.out.println();
+	}
+
+	public void ioController () {
+		int op = -1;
+		while (op != 8) {
+			printInstructions();
+			op = getInt();
+			switch (op) {
+				case 1 : setNewTemperatureRange(getInt(), getInt());
+					 break;
+				case 2 : setTableHeader(getString());
+					 break;
+				case 3 : setTableCelciusColumnHeader(getString());
+					 break;
+				case 4 : setTableFahrenheitColumnHeader(getString());
+					 break;
+				case 5 : setNumberOfColumns(getInt());
+					 break;
+				case 6 : setNumberOfSpacesBetween(getInt());
+					 break;
+				case 7 : temperatureRange.generateTable();
+					 outputTemperatureTable(temperatureRange.getTable());
+					 break;
+			}
+		}
+	}
+
+	public static void main (String args[]) {
+		new TemperatureRangeCLI().ioController();
+	}
 }
 
 
